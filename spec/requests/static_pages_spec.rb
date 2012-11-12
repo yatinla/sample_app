b610571272f3ec5b3efad2b0b4854fa9c634a11f
yatinla <mike@taylorwebhome.org>
2012-11-11 17:59:10 -0800

    Added title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,14 @@ describe "StaticPages" do
          visit '/static_pages/home'
          page.should have_selector('h1', :text => 'Sample App')
       end
-      it "should have the title 'Home'" do
+      it "should have the title" do
          visit '/static_pages/home'
-         page.should have_selector('title', :text => "#{base_title} | Home")
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
       end
-
+      it "should not have the title" do
+         visit '/static_pages/home'
+         page.should_not have_selector('title', :text => '| Home')
+      end
    end
    describe "Help page" do
       it "should have the h1 'Help'" do
